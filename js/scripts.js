a51e010b87109faec9a36f810a748859ea9dd0d3
LSanso <laurasanso22@gmail.com>
2024-04-28 23:38:33 -0300

    hoja de comidas en flexbox

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ document.querySelector("header").innerHTML = `
         <li><a href="bebidas.html">Bebidas</a></li>
         <li><a href="contacto.html">Contacto</a></li>
         <li><a href="pedidos.html">Pedidos</a></li>
-        <li><a href="about.html">Acerca de</a></li>
+        <li><a href="index.html #Nosotros">Acerca de</a></li>
     </ul>
 </nav>
 <div>Teléfono: +1 234 567 8900</div>
@@ -31,8 +31,7 @@ document.querySelector("header").innerHTML = `
 `*/
 document.querySelector("footer").innerHTML=`
 <br>
-<p>2024 Restaurante. Todos los derechos reservados.</p>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d419570.5793678844!2d-58.495246570913416!3d-34.75823392052109!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4aa9f0a6da5edb%3A0x11bead4e234e558b!2sObelisco!5e0!3m2!1ses!2sar!4v1713925122359!5m2!1ses!2sar" width=25% height=25% style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 <p>&copy; 2024 Restaurante. Todos los derechos reservados.</p>
+<a href="index.html">Inicio</a>
 <br>
 `
